a3a1307a4d54a39a15d38468ea733df7ef18483e
adieb <mrizqiamiruladieb@gmail.com>
2025-03-17 17:28:51 +0700

    feat: creating instance of ProgressBarDemo

--- src/progressbargui/Main.java
+++ src/progressbargui/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         
         // progress bar = Visual aid to let the user know that an operation is processing
+        
+        //instance
+        ProgressBarDemo demo = new ProgressBarDemo();
     }
     
 }
---
+++ src/progressbargui/ProgressBarDemo.java
@@ -0,0 +1,26 @@
+package progressbargui;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ProgressBarDemo {
+    
+    //declare
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+    
+    //construct
+    ProgressBarDemo() {
+        
+        bar.setValue(0);
+        bar.setBounds(0, 0, 420, 50);
+        
+        frame.add(bar);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    
+}
